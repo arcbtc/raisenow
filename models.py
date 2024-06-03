7a7306c0d0b493b2737b59015e8d18d3e6b6210d
Arc <33088785+arcbtc@users.noreply.github.com>
2024-06-03 18:42:38 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,55 @@
+# Data models for your extension
+
+from sqlite3 import Row
+from typing import Optional, List
+from pydantic import BaseModel
+from fastapi import Request
+
+from lnbits.lnurl import encode as lnurl_encode
+from urllib.parse import urlparse
+
+# Raises - called raisenow as raises is a reserved keyword in Python
+
+class CreateRaiseNowData(BaseModel):
+    wallet: Optional[str]
+    name: Optional[str]
+    description: Optional[str]
+    total: Optional[int]
+    closing_date: Optional[str]
+
+
+class RaiseNow(BaseModel):
+    id: str
+    wallet: Optional[str]
+    name: Optional[str]
+    description: Optional[str]
+    total: Optional[int]
+    closing_date: Optional[str]
+    lnurlpay: Optional[str]
+
+    @classmethod
+    def from_row(cls, row: Row) -> "raisenow":
+        return cls(**dict(row))
+
+# Participants
+
+class CreateParticipantData(BaseModel):
+    wallet: Optional[str]
+    name: Optional[str]
+    raisenow: Optional[str]
+    description: Optional[str]
+    total: Optional[int]
+
+
+class Participant(BaseModel):
+    id: str
+    wallet: Optional[str]
+    name: Optional[str]
+    raisenow: Optional[str]
+    description: Optional[str]
+    total: Optional[int]
+    lnurlpay: Optional[str]
+
+    @classmethod
+    def from_row(cls, row: Row) -> "raisenow":
+        return cls(**dict(row))
